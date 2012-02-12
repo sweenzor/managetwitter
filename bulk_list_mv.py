35024e48a95d278e7b43097e01258cacb4bb1312
Matt Sweeney <matt@zephyr.(none)>
2012-02-11 18:52:52 -0800

    Add comments and expand error output

--- bulk_list_mv.py
+++ bulk_list_mv.py
@@ -1,27 +1,35 @@
 import tweepy
 import oauth
 
+"""Script to move everyone you're following onto a list"""
+
 api = oauth.create()
 
+# pull list of friends
 friends_list = api.friends_ids()
 num_of_friends = len(friends_list)
 
+# figure out how many lists you need
 print 'number of friends: ', num_of_friends
 num_of_lists = int(round(num_of_friends / 400.0))
 print 'number of lists needed: ', num_of_lists
 
+# init
 root_name = 'Purge'
 lists = [] #lol
 
+# create enough twitter lists to hold all friends
 for i in range(num_of_lists):
     lists.append(api.create_list(name=root_name+str(i), mode='private'))
 
+# split friend list into a number of smaller lists
 buckets_o_friends = [friends_list[i:i+400] for i in range(0, len(friends_list), 400)]
 
+# print the counts of the sub-lists
 for l in buckets_o_friends:
     print len(l)
 
-fails = 0
+fails = []
 for i,friend_list in enumerate(buckets_o_friends):
 
     for friend in friend_list:
@@ -32,6 +40,6 @@ for i,friend_list in enumerate(buckets_o_friends):
 
         except:
             print '%i failed' % friend
-            fails += 1
+            fails.append(friend)
 
-print 'error count: ', fails
+print 'errors: ', fails
