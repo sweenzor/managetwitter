0c60e9fa1c994aea5e97ecacc5b0e6ddd452b180
Matt Sweeney <matt@zephyr.(none)>
2012-02-11 18:30:20 -0800

    Migrates the people you are following to lists
    Keeping in mind the 500 person list limit

--- bulk_list_mv.py
+++ bulk_list_mv.py
@@ -3,9 +3,35 @@ import oauth
 
 api = oauth.create()
 
-# If the authentication was successful, you should
-# see the name of the account print out
-print api.me().name
+friends_list = api.friends_ids()
+num_of_friends = len(friends_list)
 
-print len(api.friends_ids())
+print 'number of friends: ', num_of_friends
+num_of_lists = int(round(num_of_friends / 400.0))
+print 'number of lists needed: ', num_of_lists
 
+root_name = 'Purge'
+lists = [] #lol
+
+for i in range(num_of_lists):
+    lists.append(api.create_list(name=root_name+str(i), mode='private'))
+
+buckets_o_friends = [friends_list[i:i+400] for i in range(0, len(friends_list), 400)]
+
+for l in buckets_o_friends:
+    print len(l)
+
+fails = 0
+for i,friend_list in enumerate(buckets_o_friends):
+
+    for friend in friend_list:
+
+        try:
+            api.add_list_member(lists[i].id, friend)
+            print '%i added to %s' % (friend, lists[i].id)
+
+        except:
+            print '%i failed' % friend
+            fails += 1
+
+print 'error count: ', fails
